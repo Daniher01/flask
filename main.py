d1ae68183bd8c22055539234ec8dcc662f396b54
Daniel Hernandez <daniher02@gmail.com>
2023-03-08 17:23:02 -0300

    clase 15

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 todos = ['Comprar cafe', 'Enviar Solicitud de compra', 'Entregar Producto']
 
@@ -17,17 +21,16 @@ def index():
     user_ip = request.remote_addr
     
     response = make_response(redirect('/hello'))
+    session['user_ip'] = user_ip
     response.set_cookie('user_ip', user_ip)
     return response
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     context = {
         'user_ip': user_ip,
         'todos': todos,
     }
     
     return render_template('hello.html', **context)
-
-
